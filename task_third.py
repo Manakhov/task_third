709f81ce3f1dfb76e8aa5f45178105305a257308
Svyatoslav <manakhovsvyatoslav@gmail.com>
2020-08-14 20:09:57 +0300

    added count

--- task_third.py
+++ task_third.py
@@ -1,12 +1,24 @@
 def check(x):
     if x % 3 == 0 and x % 5 == 0:
-        return "foobar"
+        return 'foobar'
     elif x % 3 == 0:
-        return "foo"
+        return 'foo'
     elif x % 5 == 0:
-        return "bar"
+        return 'bar'
     else:
         return x
+
+
+def count(checked_array, key):
+    counter = 0
+    for value in checked_array:
+        if value == 'foobar':
+            counter = counter + 1
+        elif value == 'foo' and key == 3:
+            counter = counter + 1
+        elif value == 'bar' and key == 5:
+            counter = counter + 1
+    return counter
 
 
 array = []
@@ -15,3 +27,6 @@ while i < 101:
     array.append(check(i))
     i = i + 1
 print(array)
+print("Количество чисел, кратных 3:", count(array, 3))
+print("Количество чисел, кратных 5:", count(array, 5))
+print("Количество чисел, кратных 3 и 5:", array.count('foobar'))
